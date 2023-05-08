922de4e7ab660a19f711607caaa47eda4d994bae
HuyHoang0712 <hoangdinhhuy0712@gmail.com>
2023-05-08 20:38:55 +0700

    Update Light.js

--- SmartHomeProject_DT1/screens/Light.js
+++ SmartHomeProject_DT1/screens/Light.js
@@ -6,15 +6,20 @@ import { images, icons, COLORS } from "../constants";
 
 const Light = ({navigation, route}) => {
     
+<<<<<<< Updated upstream
     const [lightInfor, setLightInfor] = useState()
     const [deviceInfor, setDeviceInfor] = useState({name:'Light', curPower:0.6})
+=======
+    // const [lightInfor, setLightInfor] = useState()
+>>>>>>> Stashed changes
 
-    const getLightInfor = async() =>{
-        try {
-            const baseUrl = 'https://io.adafruit.com/api/v2/Huy_Hieu/feeds/';
-            const dataUrl = '/data?include=value&limit=1';
-            const url = `${baseUrl}/cambien2/${dataUrl}`; // Construct the URL
+    // const getLightInfor = async() =>{
+    //     try {
+    //         const baseUrl = 'https://io.adafruit.com/api/v2/Huy_Hieu/feeds/';
+    //         const dataUrl = '/data?include=value&limit=1';
+    //         const url = `${baseUrl}/cambien2/${dataUrl}`; // Construct the URL
       
+<<<<<<< Updated upstream
             const response = await fetch(url, {
               method: 'GET',
               headers: {
@@ -22,10 +27,20 @@ const Light = ({navigation, route}) => {
                 'X-AIO-Key': 'aio_eGsJ29OtlNTVZwh5RpCNlKFiq1AC' 
               }
             });
+=======
+    //         const response = await fetch(url, {
+    //           method: 'GET',
+    //           headers: {
+    //             accept: 'application/json',
+    //             'X-AIO-Key': 'aio_Trhz71ibB4eYM01QlLNEtcZrdjCf' 
+    //           }
+    //         });
+>>>>>>> Stashed changes
       
-            if (response.status === 200) {
-              const result = await response.json();
+    //         if (response.status === 200) {
+    //           const result = await response.json();
 
+<<<<<<< Updated upstream
               if (result && result.length > 0) {
                 const value = parseFloat(result[0].value);
                 if (!isNaN(value)) {
@@ -47,6 +62,24 @@ const Light = ({navigation, route}) => {
     useEffect(() => {
         setDeviceInfor({name:'Light', curPower:lightInfor/500})
     }, [lightInfor])
+=======
+    //           if (result && result.length > 0) {
+    //             const value = parseFloat(result[0].value);
+    //             if (!isNaN(value)) {
+    //                 setLightInfor(value)
+    //             }
+    //           }
+    //         } else {
+    //           console.error('Error:', response.status);
+    //         }
+    //       } catch (error) {
+    //         console.error('Error:', error);
+    //       }
+    // }
+    // useEffect(() => {
+    //     getLightInfor()
+    // }, [])
+>>>>>>> Stashed changes
     
 
     const deviceMode = [
