cd7493571a4c9a97b0e4afad361eba8dea09c24e
HuyHoang0712 <hoangdinhhuy0712@gmail.com>
2023-05-14 15:39:17 +0700

    remove nodejs server

--- SmartHomeProject_DT1/Controls/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const user_controller = require("../controllers/userController");
-
-router.get("/login", user_controller.user_login);
-
-module.exports = router;
