8c1aad91804cb0a2625a20bfdb114759bcb7a353
HuyHoang0712 <hoangdinhhuy0712@gmail.com>
2023-05-17 12:14:35 +0700

    a

--- SmartHomeProject_DT1/constants/index.js
+++ SmartHomeProject_DT1/constants/index.js
@@ -1,7 +1,7 @@
 import icons from "./icons";
 import images from "./images";
 import { COLORS, SIZES, FONTS } from "./theme";
-const HOST = "https://5b4e-125-235-239-130.ngrok-free.app"
+const HOST = "https://98e8-125-235-239-130.ngrok-free.app"
 
 export {
     images,
