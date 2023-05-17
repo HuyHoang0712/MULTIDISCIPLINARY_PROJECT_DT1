1e817bb91c9f33b5fb62b1e1a18dafc4a22c9129
Sup1o <119872859+Sup1o@users.noreply.github.com>
2023-05-17 18:55:40 +0700

    Update app.py

--- SmartHomeProject_DT1/API_Gateway/app.py
+++ SmartHomeProject_DT1/API_Gateway/app.py
@@ -1,48 +1,46 @@
-from flask import Flask
-from flask.json import JSONEncoder
+from flask import Flask, jsonify
 from bson import ObjectId
 from datetime import datetime, date
+import json
 
 from Database import user_db, aio_db
 
 from controllers.userController import user_bp
-
-class MongoJSONEncoder(JSONEncoder):
+from controllers.lightController import light_controll
+class MongoJSONEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, (datetime, date)):
-            return iso.datetime_isoformat(o)
+            return o.isoformat()
         if isinstance(o, ObjectId):
             return str(o)
-        else:
-            return super().default(o)
+        return super().default(o)
 
-# Setup Serverr
+# Setup Server
 HOST = '0.0.0.0'
 PORT = 8080
 
 app = Flask(__name__)
 app.json_encoder = MongoJSONEncoder
 app.register_blueprint(user_bp)
+app.register_blueprint(light_controll)
+# @app.route('/', methods=['POST'])
+# def postuser():
+#     db = user_db.get_database()
+#     collection_name = db["user"]
+#     post = {"name": "huy", "score": 5}
+#     collection_name.insert_one(post)
+#     return "Hello"
 
-@app.route('/', methods=['POST'])
-def postuser():
-    db = user_db.get_database()
-    collection_name = db["user"]
-    post = {"name": "huy", "score": 5}
-    collection_name.insert_one(post)
-    return "Hello"
+# @app.route('/light', methods=['POST'])
+# def turnOn():
+#     aio = aio_db.get_aio()
+#     data = {"value": 1}
+#     test_btn = aio.feeds('bbc-led')
+#     aio.send_data(test_btn.key, '1')
+#     print(test_btn)
 
-@app.route('/light', methods=['POST'])
-def turnOn():
-    aio = aio_db.get_aio()
-    data = {"value": 1}
-    test_btn = aio.feeds('bbc-led')
-    aio.send_data(test_btn.key, '1')
-    print(test_btn)
-    
-    return "<p>hello</p>"
-    
+#     return "<p>hello</p>"
+
 
 if __name__ == "__main__":
-    
     app.run(HOST, PORT, debug=True)
